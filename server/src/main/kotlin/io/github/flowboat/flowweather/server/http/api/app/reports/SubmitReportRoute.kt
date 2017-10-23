226864640ffea69d09e1b49fd041222b63d0fe65
nulldev <contactnulldev@gmail.com>
2017-10-23 09:18:08 -0400

    Fix server sensor-deduplication algorithm

--- server/src/main/kotlin/io/github/flowboat/flowweather/server/http/api/app/reports/SubmitReportRoute.kt
+++ server/src/main/kotlin/io/github/flowboat/flowweather/server/http/api/app/reports/SubmitReportRoute.kt
@@ -11,6 +11,7 @@ import io.github.flowboat.flowweather.server.db.models.dao.DbSensorGroup
 import io.github.flowboat.flowweather.server.db.models.tables.SensorTable
 import io.github.flowboat.flowweather.server.http.api.HttpApiSerializer
 import io.github.flowboat.flowweather.server.http.api.app.reports.model.DeviceReport
+import io.github.flowboat.flowweather.server.http.api.app.reports.model.SerializableSensor
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.ktor.pipeline.PipelineInterceptor
@@ -37,6 +38,15 @@ class SubmitReportRoute {
                 date = LocalDateTime.now().toString()
             }
 
+            //Required since DB operations are batched so we can't perform sensor dedupe in the DB
+            val sensorCache = mutableMapOf<String, DbSensor>()
+            fun sensorCacheKey(name: String, vendor: String) = "{$name} -> {$vendor}"
+            fun sensorCachePut(sensor: DbSensor) {
+                sensorCache[sensorCacheKey(sensor.name, sensor.vendor)] = sensor
+            }
+            fun sensorCacheGet(entry: SerializableSensor)
+                    = sensorCache[sensorCacheKey(entry.name, entry.vendor)]
+
             report.sensorMap.forEach { t, u ->
                 val group = DbSensorGroup.new {
                     this.report = dbReport
@@ -44,16 +54,18 @@ class SubmitReportRoute {
                 }
 
                 u.forEach { entry ->
-                    val sensor = DbSensor.find {
+                    val sensor = sensorCacheGet(entry.sensor) // Check memcache
+                            ?: DbSensor.find { // Check DB
                         SensorTable.report eq dbReport and
                                 SensorTable.name.eq(entry.sensor.name) and
                                 SensorTable.vendor.eq(entry.sensor.vendor)
-                    }.firstOrNull() ?: DbSensor.new {
+                    }.firstOrNull()
+                            ?: DbSensor.new { // New sensor
                         this.report = dbReport
                         name = entry.sensor.name
                         vendor = entry.sensor.vendor
                         power = entry.sensor.power.toDouble()
-                    }
+                    }.apply { sensorCachePut(this) } // Put new sensor in memcache
 
                     entry.value.value.forEachIndexed { index, it ->
                         DbSensorValue.new {
